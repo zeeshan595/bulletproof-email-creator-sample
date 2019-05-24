353f68b1d0a77a5e303b91f4c45357b4bb793567
Zeeshan ABid <zeeshan@pop-os.localdomain>
2019-05-24 14:01:15 +0100

    updated to use the latest packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,13 @@ const config = {
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const run = require("gulp-run");
-const tsCompiler = ts.createProject("tsconfig.json");
+const tsCompiler = ts.createProject({
+  target: "es5",
+  declaration: false,
+  noImplicitAny: false,
+  rootDir: config.input,
+  outDir: config.output
+});
 
 gulp.task("default", gulp.series(() => {
   return gulp
